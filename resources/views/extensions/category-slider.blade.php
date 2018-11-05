01d0fde9c5f9f21603e69fae401613c39b74d79d
Abdallah Ahmed <abdallah.se95@gmail.com>
2018-11-05 15:32:28 +0200

    category slider

--- resources/views/extensions/category-slider.blade.php
+++ resources/views/extensions/category-slider.blade.php
@@ -2,7 +2,9 @@
     <div class="swiper-slide">
         <img src="{{thumbnail($post->image->path, 'main-slider')}}">
         <div class="over-element bg-h">
-            <a href="javascript:void(0)" class="open" data-link="_video-popup.html">
+            <a href="javascript:void(0)" class="open"
+               data-poster="{{thumbnail($post->image->path, 'video-details')}}"
+               data-link="{{$post->media->path}}">
                 <i class="icon-play-icon"></i>
             </a>
         </div>
