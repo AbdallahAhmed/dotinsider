f2ca61490b0a01458726277c09672e7c851bfbdc
Abdallah Ahmed <abdallah.se95@gmail.com>
2018-11-12 15:33:36 +0200

    CSS

--- plugins/seasons/src/routes.php
+++ plugins/seasons/src/routes.php
@@ -9,7 +9,7 @@ Route::group([
     "middleware" => ["web", "auth:backend", "can:blocks.manage"],
     "namespace" => "Dot\\Seasons\\Controllers"
 ], function ($route) {
-    $route->group(["prefix" => "seasons"], function ($route) {
+    $route->group(["prefix" => "playlists"], function ($route) {
         $route->any('/', ["as" => "admin.seasons.show", "uses" => "SeasonsController@index"]);
         $route->any('/create', ["as" => "admin.seasons.create", "uses" => "SeasonsController@create"]);
         $route->any('/{season_id}/edit', ["as" => "admin.seasons.edit", "uses" => "SeasonsController@edit"]);
